182631b7417f2d8eef33209362fb6ccf9228b936
brunogabriel <bruno.gsantos89@gmail.com>
2016-10-25 21:38:48 -0200

    Adding calligraphy to apply typefaces

--- GamesInsideYourPocket/app/src/main/java/com/giyp/base/BaseActivity.java
+++ GamesInsideYourPocket/app/src/main/java/com/giyp/base/BaseActivity.java
@@ -1,7 +1,17 @@
 package com.giyp.base;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+import uk.co.chrisjenx.calligraphy.CalligraphyUtils;
+import uk.co.chrisjenx.calligraphy.TypefaceUtils;
 
 /**
  * Created by brunogabriel on 24/10/16.
@@ -14,6 +24,29 @@ public abstract class BaseActivity extends AppCompatActivity {
             setSupportActionBar(toolbar);
             toolbar.setTitle(title);
             getSupportActionBar().setDisplayHomeAsUpEnabled(isDisplayHomeAsUpEnabled);
+            applyTypefaceOnTitle(toolbar, title);
         }
+    }
+
+    private void applyTypefaceOnTitle(Toolbar toolbar, String title) {
+        try {
+            for (int i = 0; i < toolbar.getChildCount(); ++i) {
+                View child = toolbar.getChildAt(i);
+                if (child instanceof TextView) {
+                    Typeface mTypeface = TypefaceUtils.load(getAssets(), "fonts/Roboto/Roboto-Bold.ttf");
+                    if (((TextView) child).getText().toString().equalsIgnoreCase(title)) {
+                        CalligraphyUtils.applyFontToTextView((TextView)child, mTypeface);
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e("BaseActivity", "Fail to apply typeface on toolbar");
+        }
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
--- GamesInsideYourPocket/app/src/main/java/com/giyp/manager/App.java
+++ GamesInsideYourPocket/app/src/main/java/com/giyp/manager/App.java
@@ -2,10 +2,13 @@ package com.giyp.manager;
 
 import android.app.Application;
 
+import com.giyp.R;
 import com.giyp.dagger.component.DaggerNetComponent;
 import com.giyp.dagger.component.NetComponent;
 import com.giyp.dagger.module.AppModule;
 import com.giyp.dagger.module.NetModule;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
  * Created by brunogabriel on 25/10/16.
@@ -18,6 +21,12 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Roboto/Roboto-Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
 
         mNetcomponent = DaggerNetComponent.builder()
                 //.appModule(new AppModule(this))
